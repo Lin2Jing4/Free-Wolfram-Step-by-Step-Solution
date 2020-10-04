e2772dcc97adda631e8df4d10ec2a0dcc27e6654
Lin2Jing4 <35728132+Lin2Jing4@users.noreply.github.com>
2020-10-04 14:38:24 +0800

    addEventListener('click', query)
    When the button is clicked, fetch WolframAlpha's API and display Step-by-step solutions.

---
+++ script.js
@@ -0,0 +1,18 @@
+function query() {
+    var input = encodeURIComponent(document.getElementById('textarea').value)
+    fetch(
+        'https://cors-anywhere.herokuapp.com/' +
+        'http://api.wolframalpha.com/v2/query' +
+        '?appid=26LQEH-YT3P6T3YY9'             +
+        '&podstate=Step-by-step%20solution'    +
+        '&input=' + input
+    ).then(
+        response => response.text()
+    ).then(
+        string => string.replace(/plaintext/g, '')
+    ).then(
+        xml => document.write(xml)
+    )
+}
+
+document.getElementById('button').addEventListener('click', query)
